1d5f8fdc1f73c910e58bb8b28e25915ae34bb625
Okpeyemi <maqsoud.tawaliou@epitech.eu>
2025-03-20 17:45:00 +0100

    feat: change the resume

--- data/experience.ts
+++ data/experience.ts
@@ -8,6 +8,14 @@ export const experience = [
     technologies: ["WordPress", "PHP", "MySQL", "Figma"],
   },
   {
+    period: "Novembre 2024",
+    title: "Hackathon | SmartScop | 1ère place",
+    company: "Sèmè City",
+    description:
+      "Développement d'un système de gestion des flux d'entrée et de sortie au SCOP (Sèmè City Open Park).",
+    technologies: ["Elixir", "Next.js", "Azure DevOps", "Figma"],
+  },
+  {
     period: "Octobre 2024",
     title: "Développeur Fullstack",
     company: "Time Manager",
@@ -16,20 +24,20 @@ export const experience = [
     technologies: ["Elixir", "Vue.js", "Docker", "Figma"],
   },
   {
-    period: "Novembre 2023 - Décembre 2023",
-    title: "Développeur Web",
-    company: "Job Board",
-    description:
-      "Création d'une plateforme de recrutement en utilisant HTML, CSS, PHP et MySQL. Intégration de l'API backend au frontend.",
-    technologies: ["HTML5", "CSS3", "PHP", "MySQL"],
-  },
-  {
     period: "Avril 2024 - Mai 2024",
     title: "Développeur Frontend",
     company: "E-Commerce",
     description:
       "Développement des bases d'un site de vente en ligne en utilisant Symfony, React.js et TailwindCSS. Intégration de l’API backend au frontend.",
     technologies: ["Symfony", "React.js", "Tailwind CSS", "MySQL"],
+  },
+  {
+    period: "Novembre 2023 - Décembre 2023",
+    title: "Développeur Web",
+    company: "Job Board",
+    description:
+      "Création d'une plateforme de recrutement en utilisant HTML, CSS, PHP et MySQL. Intégration de l'API backend au frontend.",
+    technologies: ["HTML5", "CSS3", "PHP", "MySQL"],
   },
   {
     period: "Septembre 2022 - Septembre 2023",
