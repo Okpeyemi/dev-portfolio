22feb3c7222a64df890b93835a9195bc8833e16e
Maqsoud TAWALIOU <144787927+Okpeyemi@users.noreply.github.com>
2025-05-01 16:20:32 +0100

    Update projects-section.tsx

--- components/sections/projects-section.tsx
+++ components/sections/projects-section.tsx
@@ -16,7 +16,7 @@ export default function ProjectsSection({
   return (
     <SectionLayout
       sectionId={sectionId}
-      title="MY PROJECTS"
+      title="MES PROJETS"
       sectionNumber={sectionNumber}
     >
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
